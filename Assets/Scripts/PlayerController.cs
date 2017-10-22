9c7e568f6ff1ecd54de89a08b1dfa9711e26c9ef
mshanker23 <31782364+mshanker23@users.noreply.github.com>
2017-10-22 17:16:13 -0400

    Roll a Ball Tutorial pt. 7
    Added count text and win text

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -7,11 +8,20 @@ public class PlayerController : MonoBehaviour {
 
 	public float speed;
 
+    public Text countText;
+
+    public Text winText;
+
 	private Rigidbody rb;
+
+    private int count;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
 	}
 
 	void FixedUpdate ()
@@ -26,9 +36,21 @@ public class PlayerController : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other)
 	{
-		if (other.gameObject.CompareTag ("Pickup")) {
+		if (other.gameObject.CompareTag ("Pickup"))
+        {
 			other.gameObject.SetActive (false);
-		}
+            count = count + 1;
+            SetCountText();
+        }
 
 	}
+
+    void SetCountText ()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
 }
